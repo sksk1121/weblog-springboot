75e6ee915df1b2ba90235157626fb3b3e2d6fc3d
songkang <songkang@asiainfo.com>
2023-09-14 16:49:07 +0800

    init

---
+++ weblog-module-admin/src/test/java/com/sk/weblog/admin/WeblogModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
